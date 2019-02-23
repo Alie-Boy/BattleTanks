28950dd09a170d2be5bdc2c8b3f79e2b7dadc01d
CyberBlast <ashishdantesparda@gmail.com>
2019-02-23 18:04:19 +0530

    BT053: Converted Spring to C++ from Blueprints

--- BattleTanks/Source/BattleTanks/Private/Projectile.cpp
+++ BattleTanks/Source/BattleTanks/Private/Projectile.cpp
@@ -13,22 +13,22 @@ AProjectile::AProjectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
-	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>("Collision Mesh");
+	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh"));
 	SetRootComponent(CollisionMesh);
 	CollisionMesh->SetNotifyRigidBodyCollision(true);
 	CollisionMesh->SetVisibility(false);
 
-	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>("Launch Blast");
+	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Launch Blast"));
 	LaunchBlast->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement Component"));
 	ProjectileMovementComponent->bAutoActivate = false;
 
-	ImpactBlast = CreateDefaultSubobject<UParticleSystemComponent>("Impact Blast");
+	ImpactBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Impact Blast"));
 	ImpactBlast->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 	ImpactBlast->bAutoActivate = false;
 
-	ExplosionForce = CreateDefaultSubobject<URadialForceComponent>("Explosion Force");
+	ExplosionForce = CreateDefaultSubobject<URadialForceComponent>(FName("Explosion Force"));
 	ExplosionForce->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 }
 
